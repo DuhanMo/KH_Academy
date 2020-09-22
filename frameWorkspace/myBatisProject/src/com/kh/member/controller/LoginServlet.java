da0c1bb3365e04b578582047c7799c0f179346ac
DuhanMo <devduhanmo@gmail.com>
2020-09-22 21:51:25 +0900

    20200922

--- frameWorkspace/myBatisProject/src/com/kh/member/controller/LoginServlet.java
+++ frameWorkspace/myBatisProject/src/com/kh/member/controller/LoginServlet.java
@@ -1,7 +1,6 @@
-package com.kh.member.controller;
+package com.kh.controller;
 
-import com.kh.member.model.service.MemberServiceImpl;
-import com.kh.member.model.vo.Member;
+import com.kh.User;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,32 +11,36 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet("/login.me")
+@WebServlet("login")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
- doGet(request, response);
+
+        String userId = request.getParameter("userId");
+        String userPwd = request.getParameter("userPwd");
+        String userName = request.getParameter("userName");
+
+        User usr = new User();
+        User.setUserId(userId);
+        User.setUserPwd(userPwd);
+        User.setUserName(userName);
+
+        Member loginUser = new UserDao().loginCheck(usr);
+
+        if (loginUser != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("loginUser", loginUser);
+
+            response.sendRedirect(request.getContextPath());
+        } else {
+            request.setAttribute("msg", "로그인 실패");
+            RequestDispatcher error = request.getRequestDispatcher("WEB-INF/views/common/errorPage.jsp");
+            error.forward(request, response);
+
+        }
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String userId = request.getParameter("userId");
-        String userPwd= request.getParameter("userPwd");
-        System.out.println(userId + " : " + userPwd);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
+    ServletException, IOException {
 
-        Member mem = new Member();
-        mem.setUserId(userId);
-        mem.setUserPwd(userPwd);
-
-        // 결속성을 낮추기 위해
-//        Member loginUser = new MemberService().selectMember(mem);
-        Member loginUser = new MemberServiceImpl().selectMember(mem);
-        if(loginUser != null){
-            HttpSession session = request.getSession();
-            session.setAttribute("loginUser",loginUser);
-            response.sendRedirect(request.getContextPath());
-        }else {
-            request.setAttribute("msg","로그인실패");
-            RequestDispatcher error = request.getRequestDispatcher("WEB-INF/views/common/errorPage.jsp");
-            error.forward(request,response);
-        }
     }
 }
---
+++ springWorkspace/spring-core-tutorial/ch02/corePractice/src/com/kh/corePractice02_javaConfig/MemberDaoImpl.java
@@ -0,0 +1,34 @@
+package com.kh.corePractice02_javaConfig;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberDaoImpl implements MemberDao {
+	private final Map<Integer, Member> memberMap = new HashMap<>();
+	
+	public MemberDaoImpl() {
+	memberMap.put(1,  new Member(1,"user01", "pass01","홍길동"));
+	memberMap.put(2,  new Member(2,"user02", "pass02","유관순"));
+	}
+	
+	// 매개변수로 전달받은 회원 번호를 map에서 조회 후 리턴해주는 용도 
+	@Override
+	public Member selectMember(int memberSequence) {
+		return memberMap.get(memberSequence);
+	}
+	
+	// 매개변수로 전달받은 회원정보를 map에 추가하고 성공/실패 여부를 boolean으로 리턴 
+	@Override
+	public boolean insertMember(Member insertMember) {
+		int before = memberMap.size();
+		
+		memberMap.put(insertMember.getMemberSequence(), insertMember);
+		int after = memberMap.size();
+		
+		return (after > before)? true :false;
+	}
+
+}
