550bcf18fe667f4106fc65a8c02fc6a5ab301556
DuhanMo <devduhanmo@gmail.com>
2020-10-03 17:10:28 +0900

    20201003 spring

---
+++ springWorkspace/cloneProject/src/main/java/com/kh/spring/member/model/service/MemberServiceImpl.java
@@ -0,0 +1,12 @@
+package com.kh.spring.member.model.service;
+
+import com.kh.spring.member.model.vo.Member;
+
+public class MemberServiceImpl implements MemberService {
+
+	@Override
+	public Member loginMember(Member m) {
+		return null;
+	}
+
+}
---
+++ springWorkspace/springProject2/src/main/java/com/kh/spring/board/model/service/BoardServiceImpl.java
@@ -0,0 +1,46 @@
+package com.kh.spring.board.model.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.spring.board.model.dao.BoardDao;
+import com.kh.spring.board.model.vo.Board;
+import com.kh.spring.board.model.vo.PageInfo;
+
+@Service("bService")
+public class BoardServiceImpl implements BoardService {
+
+	@Autowired
+	private BoardDao bDao;
+
+	@Override
+	public int getListCount() {
+		return bDao.getListCount();
+	}
+
+	@Override
+	public ArrayList<Board> selectList(PageInfo pi) {
+		return bDao.selectList(pi);
+	}
+
+	@Override
+	public Board selectBoard(int bId) {
+		int result = bDao.updateCount(bId);
+		System.out.println(result);
+
+		if (result > 0) {
+			return bDao.selectBoard(bId);
+		} else {
+			return null;
+
+		}
+	}
+
+	@Override
+	public int insertBoard(Board b) {
+		return bDao.insertBoard(b);
+	}
+
+}
